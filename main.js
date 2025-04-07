75c982e736990be4fb8ea72de1c4d39a6bcc4495
felypen <146992891+felypen@users.noreply.github.com>
2025-04-07 08:29:13 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
+
 console.log(botoes);
 for (let i = 0; i < botoes.lengt;i++) {
-   console.log(i);
+   botoes[i].onclick = function (){
+ for (let j = 0; j < )
+ 
     
 }
 botoes[i].onclick = function () {
